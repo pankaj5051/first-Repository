31a2e5d2f3b1cf4f7a93a7ce084c5def4fcf4210
Pankaj Singh <pankajsinghmdt@gmail.com>
2024-05-01 09:05:03 +0530

    first commit

---
+++ class1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>  /// header file hai ye....jisme io ka mtlb hai input do output lo
+using namespace std; // namespace std iss lea likha kuki 4th line me bs cout se kaam chl jye otherwise hme pura line likhna pdta
+int main(){ // int store integer value and main is the function.
+    cout<<"Hello World";
+    return 0;
+}
